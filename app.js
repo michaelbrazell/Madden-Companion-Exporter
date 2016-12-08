ec2cbce9620d3c89003a0379cfc8eb7fe52699c2
Mike Murray <michaelbrazell@gmail.com>
2016-12-08 18:19:24 -0500

    Moving serviceAccount json to root and updating const to point to new locale

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./secure/madden-cfm-exporter-firebase-adminsdk-fdhnl-23ec97429e.json");
+const serviceAccount = require("./madden-cfm-exporter-firebase-adminsdk-fdhnl-23ec97429e.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
